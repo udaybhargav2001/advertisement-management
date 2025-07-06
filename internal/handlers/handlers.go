29e68476828765c120da9c406c56edd7f537d527
Chivukula Uday Bhargav <chivukula.bhargav@healthplix.com>
2025-07-07 01:14:58 +0530

    dockerfile and readme

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	services "advertisement-management/internal/services"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,4 +13,8 @@ func InitHandlers() {
 	Router.GET("/ads", services.GetAds)
 	Router.POST("/ads/click", services.SaveClick)
 	Router.GET("/ads/analytics", services.GetAnalytics)
+	//health check
+	Router.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "OK"})
+	})
 }
